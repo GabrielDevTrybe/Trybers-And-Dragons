afb1e153b31202cd0ee562dae85a6648251f2845
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2023-02-19 13:31:55 -0300

    requisito feito

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 
 class Character implements Fighter {
@@ -74,7 +74,7 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: SimpleFighter): void {
     const damage = this._strength;
     enemy.receiveDamage(damage);
   }
